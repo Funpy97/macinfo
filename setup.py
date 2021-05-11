0808cfe971ad19b2c3fb6da8914be81ada63d43d
Marino Iannarelli <73608319+Funpy97@users.noreply.github.com>
2021-05-11 10:41:27 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name='macinfo',
+    packages=['macinfo', 'macinfo.modules'],
+    version='0.0.1a',
+    license='MIT',
+    long_description='More information on: https://github.com/Funpy97/macinfo',
+    long_description_content_type='text/markdown',
+    description='',
+    author='Marino Iannarelli',
+    author_email='marinoiannarelli97@gmail.com',
+    url='https://github.com/Funpy97/macinfo',
+    download_url='https://github.com/Funpy97/macinfo/archive/v0.0.1-alpha.tar.gz',
+    keywords=['mac address', 'companies', 'whois', 'company'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+)
