e5590f37c80bdcd3b8317d644e18a29abbeb92e0
Marino Iannarelli <73608319+Funpy97@users.noreply.github.com>
2021-05-11 12:17:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from distutils.core import setup
 
 setup(
     name='macinfo',
-    packages=['macinfo', 'macinfo.modules'],
-    version='0.0.3a',
+    packages=['macinfo', 'macinfo.modules', 'macinfo.modules.data'],
+    version='0.0.4a',
     license='MIT',
     long_description='More information on: https://github.com/Funpy97/macinfo',
     long_description_content_type='text/markdown',
-    description='',
+    description='Search mac address info offline in about 29915 tech companies',
     author='Marino Iannarelli',
     author_email='marinoiannarelli97@gmail.com',
     url='https://github.com/Funpy97/macinfo',
-    download_url='https://github.com/Funpy97/macinfo/archive/v0.0.3-alpha.tar.gz',
+    download_url='https://github.com/Funpy97/macinfo/archive/v0.0.4-alpha.tar.gz',
     keywords=['mac address', 'companies', 'whois', 'company'],
     classifiers=[
         'Development Status :: 3 - Alpha',
